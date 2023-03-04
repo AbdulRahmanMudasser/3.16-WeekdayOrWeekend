4e67b35d7c473729532cbc7486602e07587cdfe8
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-04 13:17:24 +0500

    Add project files.

---
+++ WeekdayOrWeekend/Program.cs
@@ -0,0 +1,27 @@
+﻿class WeekdayOrWeekend
+{
+    static void Main(string[] args)
+    {
+        int input;
+
+        Console.Write("Enter An Integer From 1 to 7: ");
+        input = Convert.ToInt32(Console.ReadLine());
+
+        if (input == 1 || input == 2 || input == 3 || input == 4 || input == 5)
+        {
+            Console.Write("Working Day");
+        }
+
+        else if (input == 6 || input == 7)
+        {
+            Console.Write("Weekend");
+        }
+
+        else
+        {
+            Console.Write("Invalid Input!");
+        }
+    }
+    // end of Main method
+}
+// end of WeekdayOrWeekend class
